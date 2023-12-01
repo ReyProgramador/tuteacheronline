a603345ab8e5e06cb1986d562a2582fe12f0cc5f
jonathan <jonathancastro2018@yahoo.com>
2023-12-01 16:29:40 -0400

    agregando webpack encore

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('first_name')
+            ->add('last_name')
+            ->add('token')
+            ->add('country_id')
+            ->add('statu_id')
+            ->add('is_buyer')
+            ->add('isVerified')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
